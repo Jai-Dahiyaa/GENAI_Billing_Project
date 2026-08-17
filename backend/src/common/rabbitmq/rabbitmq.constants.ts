28f3b58d57d39eca476c7c9ce6dade37e1169755
Jai-Dahiyaa <virdahiya6@gmail.com>
2026-08-17 17:28:29 +0000

    feat(rabbitmq): define rabbitmq queues, message patterns, and client injection tokens

---
+++ backend/src/common/rabbitmq/rabbitmq.constants.ts
@@ -0,0 +1,14 @@
+export const RABBITMQ_QUEUES = {
+  BILLING_EVENTS: 'billing_events_queue',
+};
+
+export const RABBITMQ_PATTERNS = {
+  USER_REGISTER_OTP_SEND: 'user_register_otp_send',
+  USER_REGISTER_SUCCESSFULLY_EMAIL: 'user_successfully_register',
+  FORGORT_PASSWORD_OTP_SEND: 'forgot_Otp_Send',
+  PASSWORD_RESET_EMAIL_SEND: 'password_reset_email_send'
+};
+
+export const RABBITMQ_CLIENTS = {
+  MAIN_SERVICE: 'RABBITMQ_SERVICE_CLIENT',
+};
